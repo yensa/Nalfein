447badf7f2a959dadf01f829cccb3aa44852e943
yensa <yensadev@gmail.com>
2014-11-08 00:54:13 +0100

    First commit

---
+++ __init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+__prj__ = 'Nalfein'
+__version__ = '0.1'
+__license__ = 'Mozilla Public License 2.0'
+__author__ = 'yensa'
+__email__ = 'yensadev@gmail.com'
+__url__ = ''
+__date__ = '2014/11/08'
+
+
+from .scene import *
+from .game import *
+from .loader import *
+from .widget import *
+from .draw import *
+from .utils import *
+from .theme import *
