43e510c88c3e39495c3a4440cf971bb2f2d3266d
yensa <yensadev@gmail.com>
2014-11-08 18:42:52 +0100

    Worked on the button widget, reworked the theme class, added a Color object

---
+++ constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+LEFTMOUSEBUTTON = 1
+RIGHTMOUSEBUTTON = 2
+MIDDLEMOUSEBUTTON = 3
+MOUSEROLLUP = 4
+MOUSEROLLDOWN = 5
