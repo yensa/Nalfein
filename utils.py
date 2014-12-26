da11a71d6fb1250ebc6b2682e5c045974c62be6e
yensa <yensadev@gmail.com>
2014-12-26 19:57:00 +0100

    Reworked pretty much anything related to widgets

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@
 
 import pygame
 
+from UserList import UserList
 from collections import namedtuple
 
 
@@ -14,19 +15,34 @@ Point = namedtuple('Point', ['x', 'y'])
 Size = namedtuple('Size', ['width', 'height'])
 
 
-class Color(object):
-    def __init__(self, rgbavalue):
+class Color(UserList):
+    def __init__(self, rgbavalue=(0, 0, 0, 255)):
         if isinstance(rgbavalue, str):
             self._pygameColor = pygame.Color(rgbavalue)
         else:
             self._pygameColor = pygame.Color(*rgbavalue)
 
-    @property
-    def rgba(self):
-        return self._pygameColor
+        super(Color, self).__init__(self._pygameColor)
 
     def __repr__(self):
         return repr(self._pygameColor)
 
     def __str__(self):
         return repr(self)
+
+
+class RenderQueue(object):
+    def __init__(self, objs=[]):
+        self._objs = sorted(objs, self.__sort)
+
+    def __sort(self, a, b):
+        return a.z_index - b.z_index
+
+    def add(self, o):
+        # assert isinstance(o, Widget)
+        self._objs.append(o)
+        self._objs = sorted(self._objs, self.__sort)
+
+    def __iter__(self):
+        for i in self._objs:
+            yield i
